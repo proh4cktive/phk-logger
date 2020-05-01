565964c8313ea62bba5852101d1ef6c8c000fbad
Ben Mz <bmz@prohacktive.io>
2020-05-01 17:14:11 +0200

    Bump version: 0.1.3 → 0.1.4

--- tests/test_phk_logger.py
+++ tests/test_phk_logger.py
@@ -22,7 +22,7 @@ def log_file(request):
 
 class TestIt:
     def test_version(self):
-        assert __version__ == '0.1.3'
+        assert __version__ == '0.1.4'
 
     def test_init(self, caplog):
         logger = Logger()
